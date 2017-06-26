590b89ebc91cf21ef29799dcba37a3c00a8da94f
methods <methods@hotmail.com>
2017-06-25 19:55:02 -0700

    adding configparser to parse file for API key

--- blacklist_check.py
+++ blacklist_check.py
@@ -6,19 +6,17 @@ IP addresses. to use against aws before attaching EIP's to instances
 
 import json
 import requests
-import ConfigParser
+from configparser import ConfigParser
 from pprint import pprint
 
 
 ## API Configuration Data
-##TODO: Remove this API key from the file and utilize alterante methods for supplying
-##      the API key.
+## Parses .mxtbx file for API Key
 
 parser = ConfigParser()
-parser.read('./mxtbx')
+parser.read('.mxtbx')
 
-
-#API_KEY = 'APIKEYHERE'
+API_KEY = parser.get('mxtbx', 'key')
 API_URL = 'https://api.mxtoolbox.com/api/v1'
 
 def get_blacklist_results(ip):
@@ -32,7 +30,7 @@ def get_blacklist_results(ip):
     Returns:
         json: Returns a json dictonary
     '''
-    http_payload = {'Authorization': '{}'}.format(parser.get('mxtbx', 'key')) 
+    http_payload = {'Authorization': API_KEY }
     result = requests.get('{}/Lookup/blacklist/{}'.format(API_URL, ip), params=http_payload)
     result.raise_for_status()
     jresult = json.loads(result.text)
@@ -42,7 +40,7 @@ def main():
     blacklist_results = get_blacklist_results('IP HERE')
     if blacklist_results['Failed']:
         pprint(blacklist_results['Failed'])
-				print("This IP is blacklisted")
+	#print("This IP is blacklisted")
     else:
         return 0
 
